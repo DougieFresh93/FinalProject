7e43062d294d36663dcdd6c2a20fe2be9139bd4f
DougieFresh93 <littlegrinch294@yahoo.com>
2015-04-07 00:28:51 -0400

    Reads RSS of URL you enter

--- src/finalproject/FinalProject.java
+++ src/finalproject/FinalProject.java
@@ -1,21 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package finalproject;
 
 /**
  *
- * @author Brennan, Doug
+ * @author Doug, Brennan,
  */
-public class FinalProject {
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class FinalProject {
+    //Put this all into a GUI
     public static void main(String[] args) {
-        System.out.println("Doug is a faggot because he likes ESO");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the URL of the RSS feed you'd like to add");
+        String URL = input.next();
+        //URL to read
+        System.out.println(readRSS(URL));
+
     }
-    
+    //Function that reads the RSS
+    public static String readRSS(String url) {
+        try {
+            URL rss = new URL(url);
+            BufferedReader read = new BufferedReader(new InputStreamReader(rss.openStream()));
+            String source = " ";
+            String line;
+            //Reads lines from URL
+            while ((line = read.readLine()) != null) {
+                if (line.contains("<title>")) {
+                    int fPos = line.indexOf("<title>");
+                    String temp = line.substring(fPos);
+                   //replaces title with blank
+                     //sometimes depending on the size of 
+                     //the blank some of the title might be cut off no idea why
+                    temp = temp.replace("<title>", "             ");
+                    int lPos = line.indexOf("</title>");
+                   //replaces blank with whats betwenn Opening and
+                   //closing tags 
+                    temp = temp.substring(0, lPos);
+                    source += temp + "\n";
+                }
+            }
+            read.close();
+            return source;
+            //Catches if URL is not a valid URL
+        } catch (MalformedURLException ue) {
+            System.out.println("Not a valid URL");
+            //catches if somthing goes wrong reading RSS
+        } catch (IOException ioe) {
+            System.out.println("Error in reading RSS");
+        }
+        return null;
+    }
+
 }
