db20269f936118417914648707e7c2034e2cc43d
DougieFresh93 <littlegrinch294@yahoo.com>
2015-04-26 02:42:35 -0400

    Implemented In JFrame

--- src/finalproject/FinalProject.java
+++ src/finalproject/FinalProject.java
@@ -11,42 +11,46 @@ import javax.swing.JFrame;
 import javax.swing.JComponent;
 
 public class FinalProject {
+
     //Put this all into a GUI
+
     public static void main(String[] args) {
         GUI frame = new GUI();
         frame.setVisible(true);
-        
-       /* Scanner input = new Scanner(System.in);
-        System.out.println("Enter the URL of the RSS feed you'd like to add");
-        String URL = input.next();
-        //URL to read
-        System.out.println(readRSS(URL));*/
 
+        /* Scanner input = new Scanner(System.in);
+         System.out.println("Enter the URL of the RSS feed you'd like to add");
+         String URL = input.next();
+         //URL to read
+         System.out.println(readRSS(URL));*/
     }
+
     //Function that reads the RSS
-   /* public static String readRSS(String url) {
+
+    public static String readRSS(String url) {
         try {
             URL rss = new URL(url);
+            String source;
             BufferedReader read = new BufferedReader(new InputStreamReader(rss.openStream()));
-            String source = " ";
+            source = " ";
             String line;
             //Reads lines from URL
             while ((line = read.readLine()) != null) {
                 if (line.contains("<title>")) {
                     int fPos = line.indexOf("<title>");
                     String temp = line.substring(fPos);
-                   //replaces title with blank
-                     //sometimes depending on the size of 
-                     //the blank some of the title might be cut off no idea why
+                        //replaces title with blank
+                    //sometimes depending on the size of
+                    //the blank some of the title might be cut off no idea why
                     temp = temp.replace("<title>", "        ");
                     int lPos = line.indexOf("</title>");
-                   //replaces blank with whats betwenn Opening and
-                   //closing tags 
+                        //replaces blank with whats betwenn Opening and
+                    //closing tags
                     temp = temp.substring(0, lPos);
                     source += temp + "\n";
                 }
+
             }
-            read.close();
             return source;
             //Catches if URL is not a valid URL
         } catch (MalformedURLException ue) {
@@ -56,6 +60,6 @@ public class FinalProject {
             System.out.println("Error in reading RSS");
         }
         return null;
-    }*/
+    }
 
 }
